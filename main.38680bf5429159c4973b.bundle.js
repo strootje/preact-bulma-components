1f36a041cea848808ab67347ada803ca0d86501d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-18 19:17:29 +0000

    Deploy strootje/preact-bulma-components to github.com/strootje/preact-bulma-components.git:gh-pages

---
+++ main.38680bf5429159c4973b.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{414:function(n,o,c){c(415),c(508),n.exports=c(896)},436:function(n,o){},508:function(n,o,c){"use strict";c.r(o);c(509),c(676),c(680),c(884)}},[[414,1,2]]]);
