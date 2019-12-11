96f7ac8aeb3f252057e92bbcfb23a53675394d73
Bas Stroosnijder <b.stroosnijder@cargoguide.com>
2019-12-11 13:41:38 +0100

    small fixes to elementbuilder

--- core/lib/ElementBuilder.ts
+++ core/lib/ElementBuilder.ts
@@ -1,13 +1,14 @@
 import ClassNames from 'classnames';
 import { FunctionalComponent, h, JSX, RenderableProps, VNode } from 'preact';
 
+type AllowedHtmlElements = keyof JSX.IntrinsicElements;
 type Props<P> = RenderableProps<P>;
-type Attribs<A extends EventTarget> = JSX.DOMAttributes<A>;
+type Attribs<A extends EventTarget> = JSX.DOMAttributes<A> & JSX.HTMLAttributes & JSX.SVGAttributes;
 type ComponentProps<P, A extends EventTarget> = Props<P> & Attribs<A>;
 interface Component<P, A extends EventTarget> extends FunctionalComponent<ComponentProps<P, A>> { };
 
 interface ListOfClasses { [_: string]: boolean; }
-type ListOfAttribs<A extends EventTarget> = { [_ in keyof A]?: any; }
+type ListOfAttribs<A extends EventTarget> = { [Key in keyof A]?: A[Key]; }
 
 interface ClassListBuilder<P, A extends EventTarget> {
 	(props: ComponentProps<P, A>): ListOfClasses;
@@ -24,7 +25,7 @@ interface RenderComponent<P, A extends EventTarget> {
 interface ElementBuilderOpts<P, A extends EventTarget> {
 	attribs?: AttribListBuilder<P, A>;
 	classes?: ClassListBuilder<P, A>;
-	render?: string | RenderComponent<P, A>;
+	render?: AllowedHtmlElements | RenderComponent<P, A>;
 }
 
 export function ElementBuilder<P = {}, A extends EventTarget = HTMLDivElement>(opts: ElementBuilderOpts<P, A>): Component<P, A>;
