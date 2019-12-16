0baad393173350dafdd27ed38297cbb65d18db9b
Bastiaan Stroosnijder <bas@strootje.com>
2019-12-16 18:39:02 +0100

    added container element

---
+++ core/lib/Layout/Container.ts
@@ -0,0 +1,17 @@
+import { ElementBuilder } from "../ElementBuilder";
+import { ModifierProps } from "../Modifiers";
+import { PropBuilder } from "../PropBuilder";
+
+export interface ContainerProps extends ModifierProps {
+	fluid?: boolean;
+	widescreen?: boolean;
+	fullhd?: boolean;
+}
+
+export const Container = ElementBuilder<ContainerProps>('container', {
+	classes: props => ({
+		...PropBuilder(props.fluid, 'is-fluid'),
+		...PropBuilder(props.widescreen, 'is-widescreen'),
+		...PropBuilder(props.fullhd, 'is-fullhd')
+	})
+});
---
+++ core/test/Layout/Container.test.tsx
@@ -0,0 +1,41 @@
+import { expect, use } from 'chai';
+import { describe, it } from 'mocha';
+import { h } from 'preact';
+import JsxPlugin from 'preact-jsx-chai';
+import { Container } from '../../lib/Layout/Container';
+
+describe('<Container />', () => {
+	use(JsxPlugin);
+
+	it('Should render with `.container` when no props are set', () => {
+		expect(
+			<Container />
+		).to.equal(
+			<div class='container'></div>
+		);
+	});
+
+	it('Should render with `.container .is-fluid` when prop `fluid` is set to `true`', () => {
+		expect(
+			<Container fluid />
+		).to.equal(
+			<div class='container is-fluid'></div>
+		);
+	});
+
+	it('Should render with `.container .is-widescreen` when prop `widescreen` is set to `true`', () => {
+		expect(
+			<Container widescreen />
+		).to.equal(
+			<div class='container is-widescreen'></div>
+		);
+	});
+
+	it('Should render with `.container .is-fullhd` when prop `fullhd` is set to `true`', () => {
+		expect(
+			<Container fullhd />
+		).to.equal(
+			<div class='container is-fullhd'></div>
+		);
+	});
+});
